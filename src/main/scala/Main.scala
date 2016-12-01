0cdcb553b091c4f620959c29e147a7584c32447e
Fred Wang <namelos@outlook.com>
2016-12-01 22:44:46 +0800

    finish parking

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,21 @@
-object Main {
-  def compare(park1: ParkingLot, park2: ParkingLot) =
-    if (park1.distance < park2.distance)
-      park1
-    else
-      park2
+import java.util.UUID
+
+class ParkingLot(slots: Int = 4) {
+  var cars: List[(UUID, Car)] = List()
+
+  def park(car: Car) =
+    if (emptySlots > 0) {
+      val token = UUID.randomUUID()
+      cars = (token, car) :: cars
+      token
+    } else throw new Exception("There is no room.")
+
+  def withdraw(token: UUID) = cars.find(_._1 == token) match {
+    case Some((_, car)) => car
+    case None           => throw new Exception("No such car.")
+  }
+
+  def emptySlots = slots - cars.length
 }
 
-case class ParkingLot(distance: Int)
+class Car
