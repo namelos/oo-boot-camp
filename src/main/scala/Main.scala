471e27cef5aa986b21c50386617c83820c0c6dbc
Yifan Wang <namelos@outlook.com>
2016-12-22 00:21:50 +0800

    add manager test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,13 +7,14 @@ trait Pickable {
 
 trait Parkable {
   def park(car: Car): Option[UUID]
+  def canPark: Boolean
 }
 
 class ParkingLot(slots: Int = 1) extends Pickable with Parkable {
   var cars: List[(UUID, Car)] = List()
 
   def park(car: Car): Option[UUID] = cars match {
-    case cs if notFull =>
+    case cs if canPark =>
       val id = UUID.randomUUID()
       cars = (id, car) :: cs
       Some(id)
@@ -29,25 +30,33 @@ class ParkingLot(slots: Int = 1) extends Pickable with Parkable {
     }
   }
 
-  def notFull: Boolean = cars.length != slots
+  def canPark: Boolean = cars.length != slots
   def availableSlots: Int = slots - cars.length
   def emptyRate: Double = availableSlots.toDouble / slots.toDouble
   def findLot(car: Car): Option[ParkingLot] = Some(this)
 }
 
 class ParkingBoy(val parkingLots: ParkingLot*) extends Pickable with Parkable {
-  def park(car: Car): Option[UUID] = parkingLots find(_ notFull) flatMap(_ park car)
+  def park(car: Car): Option[UUID] = parkingLots find(_ canPark) flatMap(_ park car)
   def pick(id: UUID): Option[Car] = parkingLots flatMap(_ pick id) headOption
+  def canPark: Boolean = parkingLots.exists(_ canPark)
 }
 
 class SmartParkingBoy(val parkingLots: ParkingLot*) extends Pickable with Parkable {
   def park(car: Car): Option[UUID] = (parkingLots sortBy (_ availableSlots) lastOption) flatMap(_ park car)
   def pick(id: UUID): Option[Car] = parkingLots flatMap(_ pick id) headOption
+  def canPark: Boolean = parkingLots.exists(_ canPark)
 }
 
 class SuperParkingBoy(val parkingLots: ParkingLot*) extends Pickable with Parkable {
   def park(car: Car): Option[UUID] = (parkingLots sortBy (_ emptyRate) lastOption) flatMap(_ park car)
   def pick(id: UUID): Option[Car] = parkingLots flatMap(_ pick id) headOption
+  def canPark = parkingLots.exists(_ canPark)
+}
+
+class Manager[T <: Pickable with Parkable](val pickParkables: T*) {
+  def park(car: Car): Option[UUID] = pickParkables.find(_ canPark) flatMap(_ park car)
+  def pick(id: UUID): Option[Car] = pickParkables flatMap(_ pick id) headOption
 }
 
 class Car
--- src/test/scala/ManagerSpec.scala
+++ src/test/scala/ManagerSpec.scala
@@ -3,11 +3,73 @@ import java.util.UUID
 import org.scalatest.{FlatSpec, Matchers}
 
 class ManagerSpec extends FlatSpec with Matchers {
-  "parking boy" should "park a car" in {
-//    val lot = new ParkingLot
-//    val manager = new Manager(lot)
-//
-//    val token = manager
+  "manager" should "park a car to a lot" in {
+    val lot = new ParkingLot
+    val manager = new Manager(lot)
+    val car = new Car
+
+    val token = manager park car
+
+    token flatMap(lot pick) shouldBe Some(car)
+  }
+
+  it should "pick a car from a lot" in {
+    val lot = new ParkingLot
+    val car = new Car
+    val token = lot park car
+    val manager = new Manager(lot)
+
+    token flatMap(manager pick) shouldBe Some(car)
+  }
+
+
+  it should "park a car to a parking boy" in {
+    val boy = new ParkingBoy(new ParkingLot)
+    val manager = new Manager(boy)
+    val car = new Car
+
+    val token = manager park car
+
+    token flatMap(boy pick) shouldBe Some(car)
+  }
+
+  it should "pick car from parking boy" in {
+    val boy = new ParkingBoy(new ParkingLot)
+    val manager = new Manager(boy)
+    val car = new Car
+
+    val token = boy park car
+
+    token flatMap(manager pick) shouldBe Some(car)
+  }
+
+  it should "park a car to a lot and a boy" in {
+    val lot = new ParkingLot
+    val boy = new ParkingBoy(new ParkingLot)
+    val car = new Car
+    val manager = new Manager(lot, boy)
+
+    val token = manager park car
+
+    val carMayFromLot = token flatMap(lot pick)
+    val carMayFromBoy = token flatMap(boy pick)
+
+    (carMayFromLot ++ carMayFromBoy).headOption shouldBe Some(car)
+  }
+
+  it should "pick a car from a lot and a boy" in {
+    val lot = new ParkingLot
+    val carInLot = new Car
+    val lotToken = lot park carInLot
+
+    val boy = new ParkingBoy(new ParkingLot)
+    val carInBoy = new Car
+    val boyToken = boy park carInBoy
+
+    val manager = new Manager(lot, boy)
+
+    lotToken flatMap(manager pick) shouldBe Some(carInLot)
+    boyToken flatMap(manager pick) shouldBe Some(carInBoy)
   }
 }
 
